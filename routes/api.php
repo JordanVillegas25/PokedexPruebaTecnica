82251e5b6358837d18e74792e41d14558f03db76
jordan <jordanvillegas25@gmail.com>
2022-04-20 01:03:53 -0600

    terminada el backend de favoritos

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
-
+use App\Http\Controllers\Api\FavoriteController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -22,6 +22,10 @@ Route::group(['middleware' => ["auth:sanctum"]], function () {
 
     Route::get('user-profile', [UserController::class, 'userProfile']);
     Route::get('logout', [UserController::class, 'logout']);
+
+    Route::post('get-favorites',[FavoriteController::class, 'getFavorites']);
+    Route::post('register-favorites',[FavoriteController::class, 'registerFavorites']);
+    Route::post('delete-favorites',[FavoriteController::class, 'deleteFavorites']);
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
